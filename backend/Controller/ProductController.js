0f0e5593397c4e2626f21565b9383ddca19caa52
Eesha Azhar <eeshaazhar.ea@gmail.com>
2024-05-17 13:41:25 +0500

    Create ProductController.js

---
+++ backend/Controller/ProductController.js
@@ -0,0 +1,73 @@
+const Product = require("../models/product");
+
+
+const newprod = async (req,res) => {
+    console.log(req.body)
+    const prod = new Product({
+        title: req.body.title,
+        price: req.body.price,
+        stock: req.body.stock,
+        category: req.body.category
+    });
+    try {
+    
+        await prod.save();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getprod= async (req, res) => {
+    try {
+      const data = await Product.find();
+      res.json(data);
+      console.log("Done")
+    } catch (err) {
+      console.error(err); // Print any errors to the console for debugging
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
+
+  const updateprod=async (req, res) => {
+    const productId = req.params._id;
+  const updatedProductData = req.body;
+
+  try {
+    // Find the product by ID and update it
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updatedProductData, { new: true });
+
+    if (updatedProduct) {
+      res.json({ success: true, message: 'Product updated successfully', updatedProduct });
+    } else {
+      res.status(404).json({ success: false, message: 'Product not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Error updating product', error: error.message });
+  }
+  };
+
+  const deleteprod=async (req, res) => {
+    try {
+      // Extract the product ID from the request parameters
+      const productId = req.params._id;
+  
+      // Use the Product model or MongoDB driver to remove the product by ID
+      // Example using Mongoose
+      const deletedProduct = await Product.findByIdAndDelete(productId);
+  
+      if (!deletedProduct) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+  
+      res.status(200).json({ message: "Product deleted successfully", deletedProduct });
+    } catch (error) {
+      console.error("Error deleting product:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+  
+
+exports.newprod = newprod;
+exports.getprod = getprod;
+exports.updateprod = updateprod;
+exports.deleteprod = deleteprod;
